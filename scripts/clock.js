52e063faa766254c1e37a67f71abdb05bd8b6cee
froggy-jpg <meowiemeowie@proton.me>
2024-11-14 22:47:18 +0300

    initial markdowns of where bookmarks will go

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,13 +1,14 @@
 const displayDate = setInterval(() => {
     const date = new Date()
     const day = date.getDay()
-    const dayOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+    const dayOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
     const today = dayOfWeek[day]
 
     document.getElementsByClassName("date")[0].innerHTML = today
     document.getElementsByClassName("time")[0].innerHTML = date.toLocaleTimeString([], {
         hour: 'numeric',
-        minute: 'numeric'
+        minute: 'numeric',
+        second: 'numeric',
     })
 }, 1000)
 
