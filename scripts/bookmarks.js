d1b837b5d66ea1bb5f79a4a46f98bee6b342c9a8
ClaireRoush <ClaireRoush13iq@gmail.com>
2024-11-17 21:37:01 +0200

    feat: bookmarks display functionality

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -1,8 +1,7 @@
-let bookmarks = localStorage.getItem(`bookmarks`)
+let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
 
-if (!bookmarks) {
-    bookmarksArray = []
-    localStorage.setItem(`bookmarks`, JSON.stringify(bookmarksArray))
+if (!bookmarks.length) {
+    localStorage.setItem(`bookmarks`, JSON.stringify(bookmarks))
 }
 
 async function createBookmark() {
@@ -23,7 +22,7 @@ async function createBookmark() {
             return
         }
 
-        const bookmark = {
+        let bookmark = {
             id: Math.random().toString(5).substring(2,9),
             name: name,
             link: link
@@ -36,7 +35,29 @@ async function createBookmark() {
         }
 
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+        renderBookmarks()
 }
+
+
+function renderBookmarks() {
+    const getBookmarksContainer = document.getElementById('bookmarkContainer');
+    document.getElementById('bookmarkContainer').innerHTML="";
+    let getUpdatedBookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+
+    getUpdatedBookmarks.forEach(bookmark => {
+        const bookmarkDiv = document.createElement('div');
+        bookmarkDiv.className = 'bookmark';
+        bookmarkDiv.innerHTML = `
+            <p>Name: ${bookmark.name}</p>
+            <a>${bookmark.link}</a>
+        `
+        getBookmarksContainer.appendChild(bookmarkDiv)
+    })
+}
+renderBookmarks();
+
+
+
 
 const button = document.getElementById('createBookmarkBtn');
 button.addEventListener(`click`, createBookmark)
